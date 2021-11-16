78f8e78080c47baacfe505953ec3dbbdb8608da0
Clemens Schmid <clemens@nevrome.de>
2021-11-16 12:28:59 +0100

    made pedantic compiler happy

--- src/NASSA/Utils.hs
+++ src/NASSA/Utils.hs
@@ -2,7 +2,6 @@ module NASSA.Utils where
 
 import           Data.Yaml                  (ParseException, prettyPrintParseException)
 import           Control.Exception          (Exception)
-import GHC.Base (String)
 
 data NassaException =
     NassaYamlParseException FilePath ParseException -- ^ An exception to represent YAML parsing errors
